0e752790045606c70a8ee604ade230ac89dd86ec
Gabsn <gabrieldsn2006@gmail.com>
2026-05-20 12:24:59 -0300

    commit

---
+++ src/main.py
@@ -0,0 +1,67 @@
+import math
+
+
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        ra = self.find(a)
+        rb = self.find(b)
+
+        if ra == rb:
+            return False
+
+        if self.rank[ra] < self.rank[rb]:
+            self.parent[ra] = rb
+
+        elif self.rank[ra] > self.rank[rb]:
+            self.parent[rb] = ra
+
+        else:
+            self.parent[rb] = ra
+            self.rank[ra] += 1
+
+        return True
+
+
+with open("dados/entradas_do_problema.txt", "r") as f:
+    T = int(f.readline())
+
+    for _ in range(T):
+        S, P = map(int, f.readline().split())
+        points = []
+
+        for _ in range(P):
+            x, y = map(int, f.readline().split())
+            points.append((x, y))
+
+        edges = []
+
+        # cria todas as arestas possíveis
+        for i in range(P):
+            x1, y1 = points[i]
+            for j in range(i + 1, P):
+                x2, y2 = points[j]
+                dist = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                edges.append((dist, i, j))
+
+        # ordena por distância
+        edges.sort()
+        dsu = DSU(P)
+        used, answer = 0, 0
+        # Kruskal
+        for dist, u, v in edges:
+            if dsu.union(u, v):
+                answer = dist
+                used += 1
+                # parar quando restarem S componentes
+                if used == P - S:
+                    break
+        print(f"{answer:.2f}")
